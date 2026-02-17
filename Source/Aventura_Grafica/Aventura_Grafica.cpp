23280d627bff6b5a6927a80c97c8f59fa639dcd9
JoanFQ00 <joanforcadellquerol@gmail.com>
2026-02-17 22:37:44 +0100

    Project Creation Unreal Engine

---
+++ Source/Aventura_Grafica/Aventura_Grafica.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Aventura_Grafica.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aventura_Grafica, "Aventura_Grafica" );
